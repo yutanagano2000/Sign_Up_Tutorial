9aaa43a1ef335a7e432136f25cb9832025645260
yuta.nagano <yuta.nagano2000@gmail.com>
2025-11-28 14:54:18 +0900

    カードを画面中央に配置

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,60 @@
 "use client"
 
-import { FormControl, FormLabel, Input, Sheet, Typography, Button, Link } from "@mui/joy"
+import { Box, Button, FormControl, FormLabel, Input, Link, Sheet, Typography } from "@mui/joy"
 
 export default function App() {
-  return(
+  return (
+    <Box
+    sx={{
+      height:'100vh',
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center'
+    }}
+    >
     <Sheet
     sx={{
       width:300,
       mx:'auto',
       my:5,
-      px:2,
-      py:4,
+      px:4,
+      py:2,
       display:'flex',
       flexDirection:'column',
+      gap:1.5,
       borderRadius:'md',
       boxShadow:'md',
-      gap:2,
     }}
     variant="outlined">
       <div>
-        <Typography level="h3">Welcome!</Typography>
-        <Typography level="body-sm">Sign in to continue.</Typography>
+        <Typography level='h3'>Welcome!</Typography>
+        <Typography level='body-sm'>Sign up to continue</Typography>
       </div>
-      <FormControl
-      sx={{mt:0.2}}>
-        <FormLabel sx={{fontSize:'md'}}>Email</FormLabel>
+      <FormControl sx={{mt:1}}>
+        <FormLabel>Email</FormLabel>
         <Input
-        name="email"
-        type="email"
-        placeholder="email"
+        name='email'
+        type='email'
+        placeholder='enter your email'
         ></Input>
       </FormControl>
-      <FormControl
-      sx={{mt:1}}>
+      <FormControl>
         <FormLabel>Password</FormLabel>
         <Input
-        name="password"
-        type="password"
-        placeholder="enter your password"
-        ></Input>
+        name='password'
+        type='password'
+        placeholder='enter your password'>
+        </Input>
       </FormControl>
       <Button sx={{mt:1}}>Login</Button>
-      <Typography 
+      <Typography
       endDecorator={<Link href="/sign-up">Sign up</Link>}
       fontSize='sm'
+      level="body-sm"
       sx={{alignSelf:'center'}}>
         Don&apos;t have an account?
       </Typography>
-
     </Sheet>
+    </Box>
   )
 }
